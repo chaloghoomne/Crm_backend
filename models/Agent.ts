39031dae3992ab4c9528da9f23dd603e9f842cd0
abhay6969 <abhaytripathi6969@gmail.com>
2025-05-10 13:10:23 +0530

    Adding New Models

---
+++ models/Agent.ts
@@ -0,0 +1,12 @@
+import mongoose,{Schema,Document,models,model} from "mongoose"
+
+const agentSchema = new Schema({
+    companyId:{type:Schema.Types.ObjectId, ref:"Company",required:true},
+    type:{type:String,required:true},
+    name:{type:String, required:true},
+    email:{type:String,required:true},
+    mobile:{type:String,required:true},
+})
+
+const Agent = models.Agent || model<Document>("Agent",agentSchema);
+export default Agent
--- models/Lead.ts
+++ models/Lead.ts
@@ -1,31 +1,56 @@
 import mongoose,{Schema,Document,models,model} from "mongoose";
+import { ref } from "process";
 
 const LeadSchema = new Schema({
-    package:{type:Schema.Types.ObjectId,ref:'Package',required:true},
-    tourType:{type:String,required:true},
-    visaCategory:{type:Schema.Types.ObjectId,ref:'VisaCategory',required:true},
     leadBy:{type:Schema.Types.ObjectId,ref:'Employee',required:true},
-    company_id:{type:Schema.Types.ObjectId,ref:'Company',required:true},
+    companyId:{type:Schema.Types.ObjectId,ref:'Company',required:true},
+    agent:{type:Schema.Types.ObjectId,ref:'Agent'},
+    agentName:{type:String},
+    assignedEmpName:{type:String},
+    assignedEmpId:{type:Schema.Types.ObjectId,ref:'Employee'},
+    serviceType:{type:String,required:true},
     name:{type:String,required:true},
     email:{type:String,required:true},
-    visaName:{type:String,required:true},
-    validity:{type:String,required:true},
-    price:{type:Number,required:true},
-    entryType:{type:String,required:true},
     phoneNumber:{type:String,required:true},
-    gender:{type:String,required:true},
-    ageGroup:{type:String,required:true},
-    dob:{type:String,required:true},
-    fathersName:{type:String,required:true},
-    mothersName:{type:String,required:true},
-    passport:{type:String,required:true},
-    passportIssueDate:{type:String,required:true},
-    passportExpiryDate:{type:String,required:true},
-    status:{type:String,enum:['active','inactive'],default:'active'},
+    leadType:{type:String},
+    requirements:{type:String},
+    fromDate:{type:Date},
+    toDate:{type:Date},
+    followUp:{type:[Object],default:[]},
+    time:{type:String},
+    forward:{type:Boolean},
+    price:{type:Number},
+    destination:{type:String},
+
+    country:{type:String},
+    tourType:{type:String},
+    visaCategory:{type:Schema.Types.ObjectId,ref:'visaCategory'},
+    visaName:{type:String},
+    validity:{type:String,},
+
+    // departureDest:{type:String},
+    // arrivalDest:{type:String},
+  
+    adult:{type:Number},
+    child:{type:Number,default:0},
+    infant:{type:Number,default:0},
+
+    // insuranceAmount:{type:String},
+
+    hotelCategory:{type:String},
+
+    flightType:{type:String},
+
+
+    leadSource:{type:String},
+    priority:{type:String},
+
+    
+    passport:{type:String},
+    status:{type:String},
     document: {
         type: Map,
         of: String,
-        default: {},
       },      
     createdAt:{type:Date,default:Date.now},
 })
