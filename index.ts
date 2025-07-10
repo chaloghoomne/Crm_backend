1d422a74ed4f09972c90a832bdd135549f4987ba
abhay6969 <abhaytripathi6969@gmail.com>
2025-07-10 19:30:36 +0530

    more chnages

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ import { startAllCronJobs } from './cronJobs/cronScheduler';
 dotenv.config();
 
 const app = express();
-const PORT = 5174;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
